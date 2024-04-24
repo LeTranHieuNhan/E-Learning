4defebe8ad83fcdb736c88b3de6747f5603683fe
lucifer <d2f.working@gmail.com>
2024-04-24 11:11:04 +0700

    first commit

---
+++ src/test/java/org/example/e_learningback/ELearningBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.e_learningback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELearningBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
